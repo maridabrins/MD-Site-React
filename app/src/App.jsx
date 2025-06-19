7a10e163e466a86b70ff7b7423348687e79c91f3
maridabrins <maridabrins@hotmail.com>
2025-06-18 21:03:10 -0300

    feat: importando componente MainSection

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/header"
+import MainSection from "./components/mainSection"
 
 const App = () =>{
   return(
+    <>
     <Header/>
+    <MainSection/>
+    </>
   )
 }
 export default App
