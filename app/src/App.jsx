7a084d2bb8f7725248622ab741fae2ec8af63618
maridabrins <maridabrins@hotmail.com>
2025-06-18 22:01:25 -0300

    feat: adicionando componente footer

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/header"
 import MainSection from "./components/mainSection"
+import Footer from "./components/footer"
 
 const App = () =>{
   return(
     <>
     <Header/>
     <MainSection/>
+    <Footer/>
     </>
   )
 }
