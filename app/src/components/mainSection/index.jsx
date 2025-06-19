83bb6e529b77822604c7834e12b6b8ea4793e4bf
maridabrins <maridabrins@hotmail.com>
2025-06-18 21:01:22 -0300

    feat: adicionando a Main e sua estilização

---
+++ app/src/components/mainSection/index.jsx
@@ -0,0 +1,27 @@
+import ImageReact from '../../assets/images/illustrationReact.jfif'
+import './style.css'
+import Card from '../card'
+
+const MainSection = () => {
+    return(
+        <main>
+            <div className='main-container'>
+                <img src={ImageReact} alt="imagem da logo do react e icones em azul" title='imagem react' />
+            <div className='main-content'>
+                  <h2>Crie interfaces de usuário a partir de componentes</h2>
+            <p>
+                 O React permite que você crie interfaces de usuário a partir de partes individuais chamadas <mark>componentes</mark>. 
+                 Crie seus próprios componentes React, como , e . 
+                 Em seguida, combine-os em telas, páginas e aplicativos inteiros.
+            </p>
+            </div>
+            </div>
+            
+
+    
+          
+        </main>
+
+    )
+}
+export default MainSection
