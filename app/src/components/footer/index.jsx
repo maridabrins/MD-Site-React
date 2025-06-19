c32f0c4d85f72cb3181eb5ea13eb224036525460
maridabrins <maridabrins@hotmail.com>
2025-06-18 22:00:57 -0300

    feat: adicionando componente footer

---
+++ app/src/components/footer/index.jsx
@@ -0,0 +1,10 @@
+import './style.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Mariana Dabrins. Todos os direitos reservados. </p>
+        </footer>
+    )
+}
+export default Footer
